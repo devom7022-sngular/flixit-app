96e2c403e0158866aedf2f2803d254372ef260b2
Omar Marín <82827012+devom7022-sngular@users.noreply.github.com>
2023-11-11 05:01:26 -0600

    inflating views correctly(

--- app/src/main/java/com/sngular/flixitApp/ui/view/activity/Splash.kt
+++ app/src/main/java/com/sngular/flixitApp/ui/view/activity/Splash.kt
@@ -3,10 +3,14 @@ package com.sngular.flixitApp.ui.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sngular.flixitApp.R
+import com.sngular.flixitApp.databinding.ActivitySplashBinding
 
 class Splash : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
